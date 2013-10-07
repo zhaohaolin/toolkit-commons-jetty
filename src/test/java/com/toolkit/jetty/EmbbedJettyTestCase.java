67995f3a80222833543cbd2cdbed6675a79fe6fd
zhaohaolin2010 <zhaohaolin2010@gmail.com>
2013-10-07 23:17:50 +0800

    commit

---
+++ src/test/java/com/toolkit/jetty/EmbbedJettyTestCase.java
@@ -0,0 +1,26 @@
+/*
+ * CopyRight (c) 2005-2012 Taotaosou Co, Ltd. All rights reserved.
+ * Filename:    EmbbedJettyTestCase.java
+ * Creator:     joe.zhao
+ * Create-Date: 下午11:15:58
+ */
+package com.toolkit.jetty;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * TODO
+ * 
+ * @author joe.zhao
+ * @version $Id: EmbbedJettyTestCase, v 0.1 2013年10月7日 下午11:15:58 Exp $
+ */
+public class EmbbedJettyTestCase {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+	
+}
